3e21ef34df7af56ed2f8c02a58e781e90b96276a
J Lobos Vega <jalv.vg@gmail.com>
2019-07-18 22:30:17 -0400

    jsx  adn constructor funtion in place.

--- checklist/src/App.js
+++ checklist/src/App.js
@@ -1,26 +1,85 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newItem: "",
+      list: []
+    };
+  }
+
+
+  updateInput(key, value) {
+    // update react state
+    this.setState({ [key]: value });
+  }
+
+  addItem() {
+    // create a new item with unique id
+    const newItem = {
+      id: 1 + Math.random(),
+      value: this.state.newItem.slice()
+ 
+    };
+
+    // copy current list of items
+    const list = [...this.state.list];
+
+    // add the new item to the list
+    list.push(newItem);
+
+    // update state with new list, reset the new item input
+    this.setState({
+      list,
+      newItem: ""
+    });
+  }
+
+  deleteItem(id) {
+    // copy current list of items
+    const list = [...this.state.list];
+    // filter out the item being deleted
+    const updatedList = list.filter(item => item.id !== id);
+
+    this.setState({ list: updatedList });
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <div>
+        checkList
+        <br/><br/>
+          Add Item...
+          <br />
+          <input
+            type="text"
+            placeholder="Type item here:"
+            value={this.state.newItem}
+            onChange={e => this.updateInput("newItem", e.target.value)}/>
+          <button
+            onClick={() => this.addItem()}>
+          Add
+          </button>      
+          <ul>
+            {this.state.list.map(item => {
+              return(
+                <li key={item.id}>
+                  {item.value}
+                  <button
+                    onClick={() => this.deleteItem(item.id)}>
+                      X
+                  </button>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
